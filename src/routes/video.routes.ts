42335b6e377969a2eae4c3590589cf0b4415050a
Thales Morato <tha.s.morato@gmail.com>
2021-11-21 18:44:08 -0300

    refact: get by id middleware

--- src/routes/video.routes.ts
+++ src/routes/video.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { SearchByIdController } from "../modules/videos/useCases/searchById/searchByIdController";
 import { ListDataController } from "../modules/videos/useCases/listData/listDataController";
+import { SearchByIdCacheMiddleare } from "../modules/videos/useCases/searchById/searchByIdCacheMiddleware";
 
 const videoRouter: Router = Router();
 const listDataController = new ListDataController();
 const searchByIdController = new SearchByIdController();
+const searchByIdCacheMiddleare = new SearchByIdCacheMiddleare();
 
 videoRouter.get("/", listDataController.handle);
-videoRouter.get("/:id", searchByIdController.handle);
+videoRouter.get("/:id", searchByIdCacheMiddleare.handle, searchByIdController.handle);
 
 export { videoRouter };
