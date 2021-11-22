75fecd1fcce69b4aa16bf6e71c88dad0f2f9d172
Thales Morato <tha.s.morato@gmail.com>
2021-11-22 09:13:03 -0300

    refact: login responsability

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { Request, Response } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
@@ -5,9 +6,11 @@ export class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, login, password } = request.body;
 
+    const hashPassword = await hash(password, 8);
+
     const createUserUseCase = await CreateUserUseCase.getInstance();
 
-    await createUserUseCase.execute({ name, login, password });
+    await createUserUseCase.execute({ name, login, password: hashPassword });
     return response.status(201).send();
   }
 }
