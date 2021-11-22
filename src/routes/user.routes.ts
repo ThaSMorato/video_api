f1c602e9a7f0ee5fe824467dec1509087d7361de
Thales Morato <tha.s.morato@gmail.com>
2021-11-22 09:15:53 -0300

    refact: user routes with favorites

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateController";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { FavoriteController } from "../modules/accounts/useCases/favorite/FavoriteController";
+import { ensureAuthenticated } from "../shared/middlewares/EnsureAuthenticated";
 
 const userRouter = Router();
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
+const favoriteController = new FavoriteController();
 
 userRouter.post("/user", createUserController.handle);
-userRouter.post("/login", createUserController.handle);
+userRouter.post("/login", authenticateUserController.handle);
+userRouter.patch("/user/favorite", ensureAuthenticated, favoriteController.handle);
+userRouter.get("/user/favorite", ensureAuthenticated, favoriteController.handle);
 
 export { userRouter };
