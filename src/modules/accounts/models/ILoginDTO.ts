ff1fb8647fb78a6761a90bb4bb2e5289a450dbec
Thales Morato <tha.s.morato@gmail.com>
2021-11-22 09:11:42 -0300

    refact: login responsability

--- src/modules/accounts/models/ILoginDTO.ts
+++
@@ -1,4 +0,0 @@
-export interface ILoginDTO {
-  login: string;
-  password: string;
-}
--- src/modules/accounts/models/ILoginResponse.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "../entities/User";
-
-export interface ILoginResponse {
-  user: User;
-  jwt_token: string;
-  refresh_token: string;
-}
