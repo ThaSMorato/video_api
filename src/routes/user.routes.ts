d0f70098ec7b8b6ff4f003d7d29b001c9795d37b
Thales Morato <tha.s.morato@gmail.com>
2021-11-22 09:30:43 -0300

    refact: corretly user favorite list

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateController";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { FavoriteController } from "../modules/accounts/useCases/favorite/FavoriteController";
+import { ListFavoriteController } from "../modules/accounts/useCases/listFavorite/listFavoriteController";
 import { ensureAuthenticated } from "../shared/middlewares/EnsureAuthenticated";
 
 const userRouter = Router();
@@ -9,10 +10,11 @@ const userRouter = Router();
 const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
 const favoriteController = new FavoriteController();
+const listFavoriteController = new ListFavoriteController();
 
 userRouter.post("/user", createUserController.handle);
 userRouter.post("/login", authenticateUserController.handle);
 userRouter.patch("/user/favorite", ensureAuthenticated, favoriteController.handle);
-userRouter.get("/user/favorite", ensureAuthenticated, favoriteController.handle);
+userRouter.get("/user/favorite", ensureAuthenticated, listFavoriteController.handle);
 
 export { userRouter };
