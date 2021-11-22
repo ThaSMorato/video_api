aabe4fc196f8d97cb000893c54d11cf66021c743
Thales Morato <tha.s.morato@gmail.com>
2021-11-22 08:02:23 -0300

    feat: user router

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+
+const userRouter = Router();
+
+const createUserController = new CreateUserController();
+
+userRouter.post("/user", createUserController.handle);
+userRouter.post("/login", createUserController.handle);
