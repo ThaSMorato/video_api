44db6ab0e679a91f0288fbb25df8bc7004b144f1
Thales Morato <tha.s.morato@gmail.com>
2021-11-22 09:12:05 -0300

    refact: login responsability

--- src/modules/accounts/repositories/IUserRepository.ts
+++ src/modules/accounts/repositories/IUserRepository.ts
@@ -1,10 +1,9 @@
 import { User } from "../entities/User";
-import { ILoginDTO } from "../models/ILoginDTO";
-import { ILoginResponse } from "../models/ILoginResponse";
 import { IUserDTO } from "../models/IUserDTO";
 
 export interface IUserRepository {
   create(data: IUserDTO): Promise<void>;
-  login(data: ILoginDTO): Promise<ILoginResponse>;
   findByLogin(login: string): Promise<User>;
+  findById(id: string): Promise<User>;
+  updateFavorites(id: string, favorites: string[]): Promise<void>;
 }
